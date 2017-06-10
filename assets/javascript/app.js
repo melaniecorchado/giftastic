facd33dc4a486f749c7f3c45543292020c3f7814
Melanie Corchado <melaniecorchado@Melanies-MacBook-Pro.local>
2017-06-10 08:35:35 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
-    var tvshows = ["Supernatural", "Hells Kitchen", "Friends", "Big Bang Theory", "Game Of Thrones"];
+    var tvshows = ["Supernatural", "New Girl", "Friends", "Big Bang Theory", "Game Of Thrones"];
+    var stillImg = [];
+    var animateImg = [];
+    var ifStill =[true, true, true, true, true, true, true, true, true, true];
 
     function showGif(){
 
@@ -14,8 +17,13 @@ $(".television").click(function(){
           method: "GET"
           }).done(function(response) {
           	for(var j = 0; j < 10; j++){
-          	$("#gif").append("<img src='"+response.data[j].images.fixed_height.url+"'>");
+    
+          	$("#gif").append("<div class='display'><img src='"+response.data[j].images.fixed_height_still.url+"'data-show = '"+j+"' class='jifs'><p>Rating: "+response.data[j].rating
+              +"</p></div>" );
+            animateImg[j]=response.data[j].images.fixed_height.url;
+            stillImg[j]=response.data[j].images.fixed_height_still.url;
           }
+          animate();
       });
       });
 }
@@ -50,8 +58,20 @@ showGif();
 
       });
 
-      // Generic function for displaying the movieInfo
-      //$(document).on("click", ".television", );
+   function animate() {
+      $(".jifs").on("click", function(){
+        var index = $(this).attr("data-show");
+        if (ifStill[index] === true){
+          $(this).attr("src", animateImg[index]);
+          ifStill[index] = false;
+        } 
+        else {
+          $(this).attr("src", stillImg[index]);
+            ifStill[index] = true;
+        }
+      });
+
+   } 
 
 
 });
